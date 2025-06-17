dd077feab96eaac8451ee9d230434e78b1e5ec9e
ogzCode <oguzguc44@gmail.com>
2025-06-17 17:47:50 +0300

    Initialize theme on app mount using useTheme composable in app.vue

--- app.vue
+++ app.vue
@@ -1,3 +1,14 @@
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { useTheme } from '~/composables/useTheme'
+
+const { initTheme } = useTheme()
+
+onMounted(() => {
+  initTheme()
+})
+</script>
+
 <template>
     <NuxtLayout>
         <NuxtPage />
