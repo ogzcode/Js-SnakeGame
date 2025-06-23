52641f0b3cf0e0d1f1e7159be253df70dca6e75b
ogzCode <oguzguc44@gmail.com>
2025-06-23 15:31:01 +0300

    Remove all game-related files and assets

--- main.js
+++
@@ -1,7 +0,0 @@
-import { Game } from "./src/module/Game.js";
-
-const context = document.querySelector("canvas").getContext("2d");
-
-const game = new Game(context);
-setInterval(() => game.frame(context), 1000 / 10);
-
